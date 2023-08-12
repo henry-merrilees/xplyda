09551fd008e8601aab57b459f2f886abfdb8e536
Henry Merrilees <hmerrilees@hmc.edu>
2023-08-12 02:50:06 -0500

    function not really necessary anymore

--- embeddings_service/proto/__init__.py
+++ embeddings_service/proto/__init__.py
@@ -1,20 +1,16 @@
 import subprocess
 
-# todo: presently the file must be run from package root, make absolute
+# This compilation step must precede the import of the generated proto modules.
+print("Generating proto files...") 
+# this is the proto file in main project root, not python root
+proto_file_path = '../proto/embedding.proto'
+subprocess.run([
+    'python', '-m', 'grpc_tools.protoc',
+    '-I../proto', '--python_out=.', '--grpc_python_out=.',
+    proto_file_path
+])
+print("Proto files generated.")
 
-def compile_proto():
-    """ This compilation step must precede the import of the generated proto modules. """
-    print("Generating proto files...") 
-    # this is the proto file in project root, not package root
-    proto_file_path = '../proto/embedding.proto'
-    subprocess.run([
-        'python', '-m', 'grpc_tools.protoc',
-        '-I../proto', '--python_out=.', '--grpc_python_out=.',
-        proto_file_path
-    ])
-    print("Proto files generated.")
-
-compile_proto()
 # this will complain until the proto files are generated
 import embedding_pb2
 import embedding_pb2_grpc
